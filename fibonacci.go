6275a5496e16ccdbfdef8772d0583060ec605afd
saad ichkarran <saad.ichkarran@epita.fr>
2026-09-18 09:15:58 +0200

    a

---
+++ fibonacci.go
@@ -0,0 +1,12 @@
+package evaluation2
+
+func Fibonacci(index int) int {
+	if index < 0 {
+		return -1
+	}
+	if index == 0 {
+		return 0
+	} else {
+		return Fibonacci(index-2) + Fibonacci(index-1)
+	}
+}
